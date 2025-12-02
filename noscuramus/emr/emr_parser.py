90407987408d90b0142b897914bafdfc76fe0b50
IMora04 <ignmorper1@alum.us.es>
2025-12-02 13:39:16 +0100

    feat: implement csv to object parser for initial EMR dataset

---
+++ noscuramus/emr/emr_parser.py
@@ -0,0 +1,28 @@
+import csv
+from datetime import datetime
+from .models import EMR
+
+def parse_csv_to_emr_list(csv_path):
+    emr_list = []
+    with open(csv_path, newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for idx, row in enumerate(reader, start=1):
+            birth_date = datetime.strptime(row['Fecha Nacimiento'], '%Y-%m-%d').date()
+            hospitalization_date = datetime.strptime(row['Fecha Ingreso'], '%Y-%m-%d').date()
+
+            emr_obj = EMR(
+                id=idx,
+                name=row['Nombre'],
+                national_id=row['DNI'],
+                social_security_number=row['NSS'],
+                sex=row['Género'],
+                postal_code=row['Código Postal'],
+                birth_date=birth_date,
+                hospitalization_date=hospitalization_date,
+                diagnosis=row['Diagnóstico'],
+                treatment=row['Tratamiento'],
+                results=row['Resultados']
+            )
+
+            emr_list.append(emr_obj)
+    return emr_list
