0230b1cbd4684530b68ab655ec576b54a6b24fd7
IMora04 <ignmorper1@alum.us.es>
2025-12-02 13:15:45 +0100

    feat: add EMR model attributes and sample dataset

--- noscuramus/emr/models.py
+++ noscuramus/emr/models.py
@@ -20,8 +20,19 @@ class EncryptedID(models.Model):
 class EMR(models.Model):
     id = models.BigAutoField(primary_key=True)
 
+    name = models.CharField(max_length=100)
+    national_id = models.CharField(max_length=12, unique=True)
+    social_security_number = models.CharField(max_length=20, unique=True)
+    sex = models.CharField(max_length=1, choices=[('M','Male'),('F','Female')])
+    postal_code = models.CharField(max_length=10)
+    birth_date = models.DateField()
+    hospitalization_date = models.DateField()
+    diagnosis = models.CharField(max_length=255)
+    treatment = models.TextField()
+    results = models.TextField()
+
     class Meta:
         abstract = True
 
     def __str__(self):
-        return f'{{}}'
+        return f"{self.nombre} - {self.dni}"
